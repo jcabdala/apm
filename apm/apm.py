2abd766b73921d674e7ab4b85e1bf2ec975f603a
Juan Abdala <abdalajc@gmail.com>
2015-08-16 19:29:14 -0300

    install function

--- apm/apm.py
+++ apm/apm.py
@@ -7,7 +7,7 @@ import git
 from git import Repo
 import sys
 import os.path
-
+from prettytable import PrettyTable
 
 def find_repositories(word):
     dircc = "https://api.github.com/search/repositories?q="+word+"+arduino+library&sort=forks&order=desc"
@@ -15,8 +15,13 @@ def find_repositories(word):
     repositories = requests.get(dircc)
     return repositories.json()
 
-#def display_repo(repositories):
-#    for rep in repositories:
+
+def display_repo(repositories):
+    t = PrettyTable(['Index', 'name', 'user',  'forks', 'starts', "last update"])
+    for rep in repositories["items"][:5]:
+        t.add_row(["1", rep["name"], rep["owner"]["login"], rep["forks"], rep["stargazers_count"], rep["updated_at"]])
+    print t
+
 
 def get_repo(repositories, index):
     myrepo = repositories["items"][index]["clone_url"]
@@ -38,13 +43,23 @@ def downloadrepo(repourl, name, user):
 
 def main(argv):
     if argv[1] == "install":
-        repositories = find_repositories(argv[3])
         #print repositories
         if argv[2] == "-lucky":
+            repositories = find_repositories(argv[3])
             repo, name, user = get_repo(repositories, 0)
             downloadrepo(repo, name, user)
             print "The example's dir name is: ", name, "-", user
-        #else:
+        else:
+            repositories = find_repositories(argv[2])
+            display_repo(repositories)
+            try:
+                num = int(raw_input('"Select librery by Index [1-5]: "'))
+            except ValueError:
+                print "Not a number"
+            repo, name, user = get_repo(repositories, num)
+            downloadrepo(repo, name, user)
+            print "The example's dir name is: ", name, "-", user
+
 
 
 main(sys.argv)
